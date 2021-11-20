d41428623addd1683f573d41613b4d9504b021ce
Phil <happy.melodist@gmail.com>
2021-11-20 16:53:22 -0600

    complete harkup and styling review

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const labels = document.querySelectorAll(".form-control label");
+// const labels = document.querySelectorAll(".form-control label");
 
 // map ---> defined function ---> defined array --->defined function grabs items from the defined array ---> then the map creates a new array with those items
 
-labels.forEach(function (label) {
-  label.innerHTML = label.innerText
-    .split("")
-    .map(
-      (letter, index) =>
-        `<span style="transition-delay: ${index * 50}ms">${letter}</span>`
-    )
-    .join("");
-});
+// labels.forEach(function (label) {
+//   label.innerHTML = label.innerText
+//     .split("")
+//     .map(
+//       (letter, index) =>
+//         `<span style="transition-delay: ${index * 50}ms">${letter}</span>`
+//     )
+//     .join("");
+// });
+
+// Amor fati
